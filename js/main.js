44565a3b97f040658cf5f623de3f5a193a71046e
Eva Prevost <fntcstudent@fntcstudents-MacBook-Air.local>
2019-11-19 18:07:35 -0800

    added java to subscribe form

--- js/main.js
+++ js/main.js
@@ -9,4 +9,20 @@ const flkty = new Flickity( elem, {
   wrapAround: true,
   freeScroll: true
   });
-});
+const submitform = document.getElementById(subscrible-form);
+event.preventDefault();
+//gets the email form the input
+const email = getEmailFromForm();
+//this validates the email
+})
+function validateEmail(email){
+  if(!email){
+    alert('invalid email')
+  }
+  else{
+    alert('Thank you for subscribing')
+  }
+}
+function getEmailFromForm(){
+  return document.getElementById('email').value;
+}
